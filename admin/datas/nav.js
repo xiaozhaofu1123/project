66ff9c506dba8e3e682f4a2fe53d35b7eb184724
xiaofu <546195642@qq.com>
2017-07-13 20:14:31 +0800

    one

---
+++ admin/datas/nav.js
@@ -0,0 +1,69 @@
+var navs = [{
+	"title": "用户管理",
+	"icon": "fa-users",
+	"spread": true,
+	"children": [{
+		"title": "律师管理",
+		"icon": "fa-user",
+		"href": "/admin/lawyer/index"
+	}, {
+		"title": "渠道管理",
+		"icon": "&#xe609;",
+		"href": "/admin/index/#"
+	}]
+},{
+	"title": "案件管理",
+	"icon": "fa-legal",
+	"spread": false,
+	"children": [{
+		"title": "案件列表",
+		"icon": "&#xe641;",
+		"href": "/admin/case/index/"
+	}, {
+		"title": "信息发布",
+		"icon": "&#xe63c;",
+		"href": "/admin/index/#"
+	}]
+}, {
+	"title": "系统管理",
+	"icon": "fa-cogs",
+	"spread": false,
+	"children": [{
+		"title": "用户管理",
+		"icon": "fa-table",
+		"href": "/admin/systems/user"
+	}, {
+		"title": "角色管理",
+		"icon": "fa-navicon",
+		"href": "/admin/systems/role"
+	}, {
+		"title": "部门管理",
+		"icon": "&#xe62a;",
+		"href": "/admin/systems/department"
+	}, {
+		"title": "机构管理",
+		"icon": "&#xe628;",
+		"href": "/admin/systems/departmentall"
+	}, {
+		"title": "日志管理",
+		"icon": "&#xe609;",
+		"href": "/admin/systems/log"
+	}]
+}, {
+	"title": "系统外链",
+	"icon": "fa-share-alt",
+	"spread": false,
+	"children": [{
+		"title": "瀛和官网",
+		"icon": "fa-bars",
+		"href": "http://www.winteam500.com"
+	},{
+		"title": "赢在线",
+		"icon": "fa-cloud",
+		"href": "http://www.yingzaixian.cn"
+	},{
+		"title": "OA办公",
+		"icon": "fa-desktop",
+		"href": "http://oa.winteam500.com"
+	}]
+}];
